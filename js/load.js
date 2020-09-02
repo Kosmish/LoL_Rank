72ba6d1b6d2605d9ca0efb4a9a2bc5e1e3259cae
ebarrio19 <ebarrio19@eagles.ewu.edu>
2020-09-02 12:24:01 -0700

    Initial Test Rank Page

---
+++ js/load.js
@@ -0,0 +1,22 @@
+class Summoner
+{
+    constructor()
+    {
+        var summoner_name = "";
+        var tier = "";
+        var lp = 0;
+        var promoGames = []; 
+    }
+}
+
+function getRank()
+{
+
+}
+
+function displayRank()
+{
+
+}
+
+$(document).ready(start);
